ae740e09b4f41d087306fe29597e28bd2c3efb85
henrymcbean <henry_mcbean@hotmail.com>
2026-02-11 20:39:55 +0000

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Vidly_Asp.Net.Mvc5.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vidly_Asp.Net.Mvc5.Startup))]
+namespace Vidly_Asp.Net.Mvc5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
