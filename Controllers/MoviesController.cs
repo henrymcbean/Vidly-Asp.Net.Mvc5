6bf73f1d42fe325fdc631f754a372b89f1710b64
henrymcbean <henry_mcbean@hotmail.com>
2026-02-11 23:10:00 +0000

    02 lesson 7.

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly_Asp.Net.Mvc5.Models;
+using Vidly_Asp.Net.Mvc5.ViewModels;
 
 namespace Vidly_Asp.Net.Mvc5.Controllers
 {
@@ -13,8 +14,19 @@ namespace Vidly_Asp.Net.Mvc5.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer() { Name = "Customer 1" },
+                new Customer() { Name = "Customer 2" }
+            };
 
-            return View(movie);
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
         }
 
     }
---
+++ ViewModels/RandomMovieViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly_Asp.Net.Mvc5.Models;
+
+namespace Vidly_Asp.Net.Mvc5.ViewModels
+{
+    public class RandomMovieViewModel
+    {
+        public Movie Movie { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
