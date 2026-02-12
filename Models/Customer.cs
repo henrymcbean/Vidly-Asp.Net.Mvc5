0d1fd05c9c4da2e8efa665cad49eb90c4425869b
henrymcbean <henry_mcbean@hotmail.com>
2026-02-12 15:48:30 +0000

    03 lesson 13, added birthday.

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -16,5 +16,6 @@ namespace Vidly_Asp.Net.Mvc5.Models
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
         public byte MembershipTypeId { get; set; }
+        public DateTime? Birthdate { get; set; } 
     }
 }
