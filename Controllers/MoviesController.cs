287e14f93aeb3b7b95c34ea05551409d853eac01
henrymcbean <henry_mcbean@hotmail.com>
2026-02-12 20:00:15 +0000

    03 lesson 14.

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,24 +11,24 @@ namespace Vidly_Asp.Net.Mvc5.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies/Random
-        public ActionResult Random()
+        private readonly ApplicationDbContext _context = new ApplicationDbContext();
+
+        protected override void Dispose(bool disposing)
         {
-            var movie = new Movie() { Name = "Shrek!" };
-            var customers = new List<Customer>
-            {
-                new Customer() { Name = "Customer 1" },
-                new Customer() { Name = "Customer 2" }
-            };
-
-            var viewModel = new RandomMovieViewModel
-            {
-                Movie = movie,
-                Customers = customers
-            };
-
-            return View(viewModel);
+            _context.Dispose();
         }
 
+        public ViewResult Index()
+        {
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
+
+            return View(movies);
+        }
+
+        public ViewResult Details(int id)
+        {
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+            return View(movie);
+        }
     }
 }   
