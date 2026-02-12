09cda8ecf4617550c36f3b98124acd6a2ec1956d
henrymcbean <henry_mcbean@hotmail.com>
2026-02-12 13:34:18 +0000

    03 lesson 12, added Name field to MembershipType replaced discount colmn in view with membership description.

---
+++ Migrations/202602121325432_AddNameToMembershipType.cs
@@ -0,0 +1,18 @@
+﻿namespace Vidly_Asp.Net.Mvc5.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddNameToMembershipType : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.MembershipTypes", "Name", c => c.String(nullable: false, maxLength: 20));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.MembershipTypes", "Name");
+        }
+    }
+}
