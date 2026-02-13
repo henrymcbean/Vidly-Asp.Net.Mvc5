50c919127952f62275fbd3dedb81e01a3eb5b2f3
henrymcbean <henry_mcbean@hotmail.com>
2026-02-13 11:57:36 +0000

    04 lesson 10.

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -30,5 +30,53 @@ namespace Vidly_Asp.Net.Mvc5.Controllers
             var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
             return View(movie);
         }
+
+        public ActionResult New()
+        {
+            var movieForm = new MovieFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+
+            return View("MovieFormView", movieForm);
+        }
+
+        public ActionResult Save(Movie movie)
+        {
+            if (movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var movieIdb = _context.Movies.Single(m => m.Id == movie.Id);
+                movieIdb.Name = movie.Name;
+                movieIdb.GenreId = movie.GenreId;
+                movieIdb.DateAdded = movie.DateAdded;
+                movieIdb.ReleaseDate = movie.ReleaseDate;
+                movieIdb.NumberInStock = movie.NumberInStock;
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Movies");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+
+            return View("MovieFormView", viewModel);
+        }
     }
 }   
---
+++ ViewModels/MovieFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly_Asp.Net.Mvc5.Models;
+
+namespace Vidly_Asp.Net.Mvc5.ViewModels
+{
+    public class MovieFormViewModel
+    {
+        public IEnumerable<Genre> Genres { get; set; }
+        public Movie Movie { get; set; }
+    }
+}
